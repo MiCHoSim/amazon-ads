a51c635e89e4310f577712f410e8259d32e642a3
Michal Šimaľa <simalmichal@gmail.com>
2023-04-14 12:18:13 +0200

    Uprava DB

--- Update Db.sql
+++ Update Db.sql
@@ -1,3 +1,7 @@
-UPDATE `article` SET `contents` = '', `description` = '' WHERE `article`.`article_id` = 1
+UPDATE `article` SET `contents` = '', `description` = '' WHERE `article`.`article_id` = 1;
 
-UPDATE `article` SET `contents` = 'About us' WHERE `article`.`article_id` = 3
+UPDATE `article` SET `contents` = 'About us' WHERE `article`.`article_id` = 3;
+
+UPDATE `article` SET `link` = 'amazon-ads/statistics' WHERE `article`.`article_id` = 4;
+
+UPDATE `article` SET `link` = 'amazon-monthly-sales/monthly-sales' WHERE `article`.`article_id` = 5;
